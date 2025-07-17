09ae730aef5d93251979cbb43d554656cad98140
hafriz <hafriz@gmail.com>
2025-07-17 14:30:36 +0800

    Create pelicanconf.py

---
+++ docs/pelicanconf.py
@@ -0,0 +1,13 @@
+import os
+
+AUTHOR = 'Bahagian Pembelajaran Digital'
+SITENAME = 'e-Learning Platform Services Documentation'
+SITEURL = ''
+
+PATH = 'docs/content'
+TIMEZONE = 'Asia/Kuala_Lumpur'
+DEFAULT_LANG = 'en'
+# Output path compatible with Read the Docs environment
+OUTPUT_PATH = 'output_html'
+DELETE_OUTPUT_DIRECTORY = True
+
