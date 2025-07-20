fc60a8ed04d3f21553fb5b5ac4e29c14ceb46186
hafriz <hafriz@gmail.com>
2025-07-20 14:11:53 +0800

    Hide project name and version in Sphinx header

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,11 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = '../_static/elearning.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
